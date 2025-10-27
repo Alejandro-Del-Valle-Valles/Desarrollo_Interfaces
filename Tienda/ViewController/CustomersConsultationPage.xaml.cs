d501427713d8da437cfa992ee8951de37f56e065
Alejandro del Valle Vallés <alejandrodelvalle06@outlook.es>
2025-10-27 20:06:44 +0100

    Frontend páginas Alta y consulta multicliente terminados

--- Tienda/ViewController/CustomersConsultationPage.xaml.cs
+++ Tienda/ViewController/CustomersConsultationPage.xaml.cs
@@ -2,6 +2,7 @@ namespace Tienda.ViewController;
 
 public partial class CustomersConsultationPage : ContentPage
 {
+    private string SelectedCity = String.Empty;
 	public CustomersConsultationPage()
 	{
 		InitializeComponent();
--- Tienda/ViewController/RegistrationPage.xaml.cs
+++ Tienda/ViewController/RegistrationPage.xaml.cs
@@ -6,4 +6,44 @@ public partial class RegistrationPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    /// <summary>
+    /// Save a new client if not exists.
+    /// </summary>
+    /// <param name="sender">The source of the event, typically the control that was clicked.</param>
+    /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+    private void OnSaveClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Modify the data of a user if exists.
+    /// </summary>
+    /// <param name="sender">The source of the event, typically the control that was clicked.</param>
+    /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+    private void OnModifyClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Delete a user if exist.
+    /// </summary>
+    /// <param name="sender">The source of the event, typically the control that was clicked.</param>
+    /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Clear the data from the entries.
+    /// </summary>
+    /// <param name="sender">The source of the event, typically the control that was clicked.</param>
+    /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+    private void OnClearClicked(object sender, EventArgs e)
+    {
+
+    }
 }
