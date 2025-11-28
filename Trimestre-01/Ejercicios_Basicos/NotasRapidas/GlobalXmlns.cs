266a68c58091565f24c2878689421d3b2c668b08
Alejandro Del Valle <alejandrodelvalle06@outlook.es>
2025-11-28 14:14:36 +0100

    Empiezo el 3 de repaso

---
+++ Trimestre-01/Ejercicios_Basicos/NotasRapidas/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "NotasRapidas")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "NotasRapidas.Pages")]
