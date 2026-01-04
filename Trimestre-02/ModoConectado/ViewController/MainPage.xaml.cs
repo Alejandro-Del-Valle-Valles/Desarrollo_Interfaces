f446420c17714419321ad5610e3e1788475017bc
Alejandro del Valle Vallés <alejandrodelvalle06@outlook.es>
2026-01-04 14:35:04 +0100

    Empiezo el ejercicio de manejo de una BBDD SQLite en modo conectado

---
+++ Trimestre-02/ModoConectado/ViewController/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace ModoConectado.ViewController
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //CRUD CALL METHODS
+        private void OnLabelSearchTapped(object? sender, TappedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnSaveClicked(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUpdateClicked(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDeleteClicked(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //FORM METHODS
+        private void OnClearClicked(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnSearchClicked(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnSurnameTapped(object? sender, TappedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
